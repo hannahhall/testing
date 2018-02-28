fa1fa042c7777f15e527d6a99855b8f6cafd4c9f
Hannah Hall <hannahkhall13@gmail.com>
2018-02-28 14:10:25 -0600

    first test

---
+++ Calculator.Tests/CalculatorShould.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using Calculator;
+
+namespace Calculator.Tests
+{
+    public class CalculatorShould
+    {
+        private Calculator _calculator;
+
+        public CalculatorShould()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Fact]
+        public void AddTwoIntegers()
+        {
+            // Given this input to the method
+            int sum = _calculator.Add(54, 29);
+
+            // We are asserting that the output should be this
+            Assert.Equal(sum, 83);
+        }
+    }
+}
